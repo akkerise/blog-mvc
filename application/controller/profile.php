1f171cd99c4afebef81a99960b36258b31568192
thuongmh91 <thuongmaihuy91@gmail.com>
2016-12-16 17:40:34 +0700

    đâsd

---
+++ application/controller/profile.php
@@ -0,0 +1,21 @@
+<?php
+
+class profile extends Controller {
+
+    public function index ()
+    {
+        $id_user = $_SESSION['id_user'];
+        $profile = $this->model->profile($id_user);
+        require APP . "view/__templates/header.php";
+        require APP . "view/profile.php";
+        require APP . "view/__templates/footer.php";
+    }
+
+    public function update_ava ()
+    {
+        if(isset($_POST['post']))
+        {
+            
+        }
+    }
+}
